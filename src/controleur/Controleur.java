3b9c022c3fae096728981f8461141c6c1cac47fb
Loïc Feuga <feuga.loic@etu.unice.fr>
2015-03-24 21:23:45 +0100

    Premier test de l'affichage de la map ok avec un txt

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -1,13 +1,18 @@
 package controleur;
 
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
+import jeu.Parser;
 import model.Direction;
 import model.Moteur;
 import model.objet.ObjetCollision;
@@ -26,9 +31,24 @@ public class Controleur {
 		//parser de la carte;
 		ArrayList<ObjetCollision> list = new ArrayList();
 		Joueur joueur = new Joueur(1,"c",new Rectangle());
-		HashMap<Point, BufferedImage> listImage = new HashMap<>();
+		//HashMap<Point, BufferedImage> listImage = new HashMap<>();
 		
-		vue = new Vue(listImage);
+		ArrayList<Image> listImage = new ArrayList<>();
+		
+		//json
+		String jsonString = VFichier.chargementFichierTexte("data/testjson.txt");
+		HashMap jsonHashMap = Parser.jsonDecoding(jsonString);
+		
+		//ajout image
+		Image image = null;
+		
+		try {image = ImageIO.read(new File("data/image/mur.png"));}
+		catch (IOException e) { e.printStackTrace();}
+		
+		listImage.add(image);
+		
+		//init
+		vue = new Vue(listImage,jsonHashMap);
 		moteur = new Moteur(list,joueur);
 		
 		ListenerTouche touche = new ListenerTouche(this);
--- src/vue/Vue.java
+++ src/vue/Vue.java
@@ -1,28 +1,72 @@
 package vue;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.HashMap;
+
+
+
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JPanel;
 
-import controleur.Controleur;
+import org.json.simple.JSONArray;
 
-public class Vue extends JPanel{
-	private HashMap<Point,BufferedImage> listImage;
+
+
+public class Vue extends JPanel implements Observer{
+	//private HashMap<Point,BufferedImage> listImage;
+	private ArrayList<Image> listImage;
+	private  HashMap<String,Object> configJSON;
 
 	/**
 	 * HashMap<Point,BufferedImage>
 	 */
-	public Vue(HashMap<Point,BufferedImage> imageList) {
+	public Vue(ArrayList<Image>imageList, HashMap<String,Object> json) {
 		super();
 		this.listImage = imageList;
+		this.configJSON = json;
 	
 	}
 	
 	@Override
 	public void paint(Graphics g) {
-		
 		super.paint(g);
+		/*
+		carte->0->x | y | largeur | hauteur | type 
+		nbCaseLargeur 
+		nbCaseHauteur
+		tailleCase
+		*/
+		
+		JSONArray list =  (JSONArray) configJSON.get("carte");
+		
+		for(int i = 0 ; i < (int) (long) configJSON.get("nbCaseLargeur") ; i++){
+			
+			for(int j = 0; j < (int) (long) configJSON.get("nbCaseHauteur") ; j++){
+				
+				HashMap<String, Long> listIn = (HashMap<String, Long>) list.get(i+j);
+				int x = (int) (long) listIn.get("x");
+				int y = (int) (long) listIn.get("y");
+				int largeur = (int) (long) listIn.get("largeur");
+				int hauteur = (int) (long) listIn.get("hauteur");
+				
+				g.drawImage(listImage.get(0), x , y, largeur , hauteur,null);
+			}
+		}
+		
+		
 	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		
+	}
+
+	
+
+
 }
