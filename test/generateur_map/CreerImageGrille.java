ac7de9ebe36dc530b4887ff29d24bc9a476a921c
Sébastien Nicoletti <nicoletti.sebastien@etu.unice.fr>
2015-03-24 21:05:35 +0100

    Ajout de la javadoc dans TestGenerateur et CreerImageGrille

--- test/generateur_map/CreerImageGrille.java
+++ test/generateur_map/CreerImageGrille.java
@@ -9,47 +9,60 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * The Class CreerImageGrille.
+ */
 public class CreerImageGrille {
+
+	/** The taille carre. */
+	public static int TAILLE_CARRE = 50;
 	
-	public static int TAILLE_CARRE = 50; 
-	public static final String NOM =  "data\\image\\map\\Map";
-	public static final String FORMAT =  "bmp";
+	/** The Constant NOM. */
+	public static final String NOM = "data\\image\\map\\Map";
 	
+	/** The Constant FORMAT. */
+	public static final String FORMAT = "bmp";
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
-		//Fond
+		// Fond
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		dim.setSize( ((int)(dim.getWidth()/TAILLE_CARRE)) * TAILLE_CARRE,
-				((int)(dim.getHeight()/TAILLE_CARRE))  * TAILLE_CARRE);
-		BufferedImage image = new BufferedImage(dim.width, dim.height, BufferedImage.TYPE_INT_RGB);
-		
-		for(int y = 0; y < image.getHeight(); y ++){
-			for( int x = 0; x < image.getWidth(); x++){
+		dim.setSize(((int) (dim.getWidth() / TAILLE_CARRE)) * TAILLE_CARRE,
+				((int) (dim.getHeight() / TAILLE_CARRE)) * TAILLE_CARRE);
+		BufferedImage image = new BufferedImage(dim.width, dim.height,
+				BufferedImage.TYPE_INT_RGB);
+
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++) {
 				image.setRGB(x, y, Color.white.getRGB());
 			}
 		}
-		
-		//Disposition des lignes
-		for( int y = 0; y < image.getHeight(); y += TAILLE_CARRE ){
-			for( int x = 0; x < image.getWidth(); x ++){
+
+		// Disposition des lignes
+		for (int y = 0; y < image.getHeight(); y += TAILLE_CARRE) {
+			for (int x = 0; x < image.getWidth(); x++) {
 				image.setRGB(x, y, Color.black.getRGB());
 			}
 		}
-		
-		for( int x = 0; x < image.getWidth(); x += TAILLE_CARRE ){
-			for( int y = 0; y < image.getHeight(); y ++){
+
+		for (int x = 0; x < image.getWidth(); x += TAILLE_CARRE) {
+			for (int y = 0; y < image.getHeight(); y++) {
 				image.setRGB(x, y, Color.black.getRGB());
 			}
 		}
-		
-		//Save
+
+		// Save
 		try {
-			ImageIO.write(image, FORMAT, new File(NOM+"."+FORMAT));
+			ImageIO.write(image, FORMAT, new File(NOM + "." + FORMAT));
 			System.out.println("Image créer !!!");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 }
--- test/generateur_map/TestGenerateur.java
+++ test/generateur_map/TestGenerateur.java
@@ -16,109 +16,179 @@ import jeu.Parser;
 import model.objet.Mur;
 import model.objet.ObjetCollision;
 
-public class TestGenerateur {	
-	
+/**
+ * The Class TestGenerateur.
+ */
+public class TestGenerateur {
+
+	/** The image. */
 	private BufferedImage image = null;
-	private int nbCaseLargeur;
-	private int nbCaseHauteur;
 	
+	/** The nb case largeur. */
+	private int nbCaseLargeur;
+	
+	/** The nb case hauteur. */
+	private int nbCaseHauteur;
+
+	/**
+	 * Instantiates a new test generateur.
+	 */
 	public TestGenerateur() {
 		initImage();
 		List<ObjetCollision> listObjetC = initListCollision();
 		String json = toJSON(listObjetC);
 		System.out.println(json);
-		
-		VFichier.sauvegardeFichierTexte(CreerImageGrille.NOM+".json", json);
+
+		VFichier.sauvegardeFichierTexte(CreerImageGrille.NOM + ".json", json);
 	}
-	
-	private void initImage(){
+
+	/**
+	 * Inits the image.
+	 * 
+	 * Permet de chercher l'image de test.
+	 * 
+	 */
+	private void initImage() {
 		try {
-			image = ImageIO.read(new File(CreerImageGrille.NOM+"."+CreerImageGrille.FORMAT));
+			image = ImageIO.read(new File(CreerImageGrille.NOM + "."
+					+ CreerImageGrille.FORMAT));
 		} catch (IOException e) {
 			System.out.println("Erreur chargement image...");
 			System.exit(1);
 		}
-		
-		nbCaseLargeur = image.getWidth() /  CreerImageGrille.TAILLE_CARRE;
-		nbCaseHauteur = image.getHeight() /  CreerImageGrille.TAILLE_CARRE;
+
+		nbCaseLargeur = image.getWidth() / CreerImageGrille.TAILLE_CARRE;
+		nbCaseHauteur = image.getHeight() / CreerImageGrille.TAILLE_CARRE;
 	}
-	
-	private List<ObjetCollision> initListCollision(){
-		List<ObjetCollision> listObjetC= new ArrayList<ObjetCollision>(); 
-		
-		for(int y = 0; y < nbCaseHauteur; y++ ){
-			for( int x = 0; x < nbCaseLargeur; x++ ){
-				int xLocation = (x*CreerImageGrille.TAILLE_CARRE);
-				int yLocation = (y*CreerImageGrille.TAILLE_CARRE);
-				int xRGB =  xLocation+ CreerImageGrille.TAILLE_CARRE/2;
-				int yRGB =  yLocation+ CreerImageGrille.TAILLE_CARRE/2;
-				
-				int rgb = image.getRGB( xRGB, yRGB);
+
+	/**
+	 * Inits the list collision.
+	 * 
+	 * Permet de créer la liste des objets collisions.
+	 *
+	 * @return the list
+	 */
+	private List<ObjetCollision> initListCollision() {
+		List<ObjetCollision> listObjetC = new ArrayList<ObjetCollision>();
+
+		for (int y = 0; y < nbCaseHauteur; y++) {
+			for (int x = 0; x < nbCaseLargeur; x++) {
+				int xLocation = (x * CreerImageGrille.TAILLE_CARRE);
+				int yLocation = (y * CreerImageGrille.TAILLE_CARRE);
+				int xRGB = xLocation + CreerImageGrille.TAILLE_CARRE / 2;
+				int yRGB = yLocation + CreerImageGrille.TAILLE_CARRE / 2;
+
+				int rgb = image.getRGB(xRGB, yRGB);
 				Color couleur = new Color(rgb);
-				
+
 				Rectangle rect = new Rectangle(xLocation, yLocation,
-						CreerImageGrille.TAILLE_CARRE, CreerImageGrille.TAILLE_CARRE);
-				
-				ObjetCollision obj = colorToObjectColision(couleur, rect);
-				if(obj != null ){
+						CreerImageGrille.TAILLE_CARRE,
+						CreerImageGrille.TAILLE_CARRE);
+
+				ObjetCollision obj = colorToObjectCollision(couleur, rect);
+				if (obj != null) {
 					listObjetC.add(obj);
 				}
 			}
 		}
-		
+
 		return listObjetC;
 	}
-	
-	private String toJSON(List<ObjetCollision> listObjetC){
+
+	/**
+	 * To json.
+	 * 
+	 * Permet de parser nos données en JSON
+	 * 
+	 * Hashmap: "nbCaseLargeur" -> long
+	 * 			"nbCaseHauteur" -> long
+	 *          "carte" -> List<Hashmap>
+	 *          	- le Hashmap: -> "x" -> long
+	 *          					 "y" -> long
+	 *          					 "largeur" -> long
+	 *          					 "hauteur" -> long
+	 *          					 "type" -> String
+	 *
+	 * @param listObjetC the list objet c
+	 * @return the string
+	 */
+	private String toJSON(List<ObjetCollision> listObjetC) {
 		HashMap<String, Object> map = new HashMap<>();
 		List<HashMap<String, Object>> objCollisions = new ArrayList<>();
-		
-		for( ObjetCollision obj : listObjetC){
-			HashMap<String, Object> descObj = new HashMap<>(); 
+
+		for (ObjetCollision obj : listObjetC) {
+			HashMap<String, Object> descObj = new HashMap<>();
 			descObj.put("x", obj.getHitBox().x);
 			descObj.put("y", obj.getHitBox().y);
 			descObj.put("largeur", obj.getHitBox().width);
 			descObj.put("hauteur", obj.getHitBox().height);
 			descObj.put("type", obj.getNom());
-			
+
 			objCollisions.add(descObj);
 		}
-		
+
 		map.put("carte", objCollisions);
 		map.put("nbCaseLargeur", nbCaseLargeur);
 		map.put("nbCaseHauteur", nbCaseHauteur);
 		map.put("tailleCase", CreerImageGrille.TAILLE_CARRE);
-		
-		
+
 		return Parser.jsonEncoding(map);
 	}
-	
-	private ObjetCollision colorToObjectColision(Color couleur, Rectangle rect){
-		if( Type.MUR.getCouleur().equals(couleur) ){
-			return new Mur( rect );
+
+	/**
+	 * Color to object collision.
+	 * 
+	 * Permet de traduire le code couleur en un objet collision.
+	 *
+	 * @param couleur the couleur
+	 * @param rect the rect
+	 * @return the objet collision
+	 */
+	private ObjetCollision colorToObjectCollision(Color couleur, Rectangle rect) {
+		if (Type.MUR.getCouleur().equals(couleur)) {
+			return new Mur(rect);
 		}
-		
+
 		return null;
 	}
-	
-	
-	public enum Type{
+
+	/**
+	 * The Enum Type.
+	 */
+	public enum Type {
+		
+		/** The mur. */
 		MUR(Color.black);
-		
+
+		/** The couleur. */
 		private Color couleur;
-		
-		Type (Color couleur) {
+
+		/**
+		 * Instantiates a new type.
+		 *
+		 * @param couleur the couleur
+		 */
+		Type(Color couleur) {
 			this.couleur = couleur;
 		}
-		
-		public Color getCouleur(){
+
+		/**
+		 * Gets the couleur.
+		 *
+		 * @return the couleur
+		 */
+		public Color getCouleur() {
 			return couleur;
 		}
 	}
-	
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		new TestGenerateur();
 	}
-	
 
 }
